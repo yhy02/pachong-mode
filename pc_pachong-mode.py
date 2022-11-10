c9e284f8a871530027fc583f2bad0cd69a35f5f5
yhy02 <18608130777@aliyun.com>
2022-11-10 15:54:03 +0800

    commit 2

--- pc_pachong-mode.py
+++ pc_pachong-mode.py
@@ -1,12 +1,23 @@
 import urllib.request
+import gevent
+from gevent import monkey
 
-def main():
-    req = urllib.request.urlopen("https://img.alicdn.com/imgextra/i3/30301515/O1CN01ce5G791N3uaiW52KX_!!0-saturn_solar.jpg_468x468q75.jpg")
+monkey.patch_all()
+
+
+def downloader(img_name, img_url):
+    req = urllib.request.urlopen(img_url)
 
     img_content = req.read()
 
-    with open("1.jpg", "wb") as f:
+    with open(img_name, "wb") as f:
         f.write(img_content)
+
+def main():
+    gevent.joinall([
+            gevent.spawn(downloader, "1.jpg", "https://img.alicdn.com/imgextra/i3/30557696/O1CN01W0QoE526iotrdQMqk_!!0-saturn_solar.jpg_468x468q75.jpg"),
+            gevent.spawn(downloader, "2.jpg", "https://img.alicdn.com/imgextra/i3/125362664/O1CN01TRAVVb1VY9uDgVKDr_!!0-saturn_solar.jpg_468x468q75.jpg")
+    ])
 
 if __name__ == "__main":
     main()
